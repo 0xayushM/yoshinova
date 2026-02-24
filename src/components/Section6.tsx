c9b063a63dd3d7149c2821282bbc7ee517f59484
0xayushm <mangal.ayush.4982@gmail.com>
2026-02-24 09:57:53 +0530

    add BESS zone cards with liquid glass effect, animated charts, 3D tilt interaction, and scroll-driven reveal animations

--- src/components/Section6.tsx
+++ src/components/Section6.tsx
@@ -1,11 +1,13 @@
 "use client";
 
 import React from 'react';
+import BESSCard from './BESSCard';
+import { telecomData } from '@/utils/bessData';
 
 const Section6 = () => {
   return (
-    <section className="w-screen h-screen flex items-center justify-center">
-      <div className="text-black text-2xl">Section 6</div>
+    <section className="w-screen h-screen relative overflow-hidden">
+      <BESSCard zone={telecomData} align="right" sectionIndex={5} />
     </section>
   );
 };
--- src/components/Section9.tsx
+++ src/components/Section9.tsx
@@ -1,11 +1,13 @@
 "use client";
 
 import React from 'react';
+import BESSCard from './BESSCard';
+import { solarData } from '@/utils/bessData';
 
 const Section9 = () => {
   return (
-    <section className="w-screen h-screen flex items-center justify-center">
-      <div className="text-black text-2xl">Section 9</div>
+    <section className="w-screen h-screen relative overflow-hidden">
+      <BESSCard zone={solarData} align="left" sectionIndex={8} />
     </section>
   );
 };
